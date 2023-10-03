aebbbd3fe1480c0881264d1d6bf1cd5fecde6c8b
norahha <norahha@stud.ntnu.no>
2023-10-03 12:12:38 +0200

    why it a submodule

---
+++ module06_isnotasubmodule/backend/providers.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.71.0"
+    }
+  }
+
+    backend "azurerm" {
+      resource_group_name = "nhhsabetw4ij33"
+      storage_account_name = "nhhsawrgpbu9kdvs6q"
+      container_name = "nhhsc8u0"
+      key = "rg.terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
