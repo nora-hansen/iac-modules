19ecb4bce720d5207af9d3447752eff620d0c578
norahha <norahha@stud.ntnu.no>
2023-10-05 11:47:12 +0200

    Gotta redo the backend

--- module07/staticwebsite/providers.tf
+++ module07/staticwebsite/providers.tf
@@ -5,12 +5,12 @@ terraform {
             version = "3.73.0"
         }
     }
-    backend "azurerm"   {
-        resource_group_name = "nhhsabetw4ij32"
-        storage_account_name = "nhhsaweb74ddfc2j"
-        container_name = "$web"
-        key = "web.terraform.nhhtfstate"
-    }
+    # backend "azurerm"   {
+    #     resource_group_name = "nhhsabetw4ij32"
+    #     storage_account_name = "nhhsaweb74ddfc2j"
+    #     container_name = "$web"
+    #     key = "web.terraform.nhhtfstate"
+    # }
 }
 
 provider "azurerm"  {
